c97ed2927214355f6420f17b79fbc21cb98ec3b5
rasim <rasimhusanbaev20@gmail.com>
2025-06-15 21:32:34 +0500

    final9

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import s from './App.scss'
+import './App.scss'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Header from './components/Header/Header'
